84ed3c3bfd8c64c6d5d535b30465a0934aaa0ede
Maxim Stovba <maxim.stovba@gmail.com>
2025-01-04 17:15:30 +0300

    feat/mongo-sharding-repl

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -13,34 +13,38 @@ EOF
 
 echo "Config server initialized."
 
-# Инициализация первого шарда
-docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+# Инициализация первого шарда с репликацией
+docker compose exec -T shard1_1 mongosh --port 27018 --quiet <<EOF
 rs.initiate({
     _id: "shard1",
     members: [
-        { _id: 0, host: "shard1:27018" }
+        { _id: 0, host: "shard1_1:27018" },
+        { _id: 1, host: "shard1_2:27018" },
+        { _id: 2, host: "shard1_3:27018" }
     ]
 });
 EOF
 
-echo "Shard1 initialized."
+echo "Shard1 replication set initialized."
 
-# Инициализация второго шарда
-docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+# Инициализация второго шарда с репликацией
+docker compose exec -T shard2_1 mongosh --port 27019 --quiet <<EOF
 rs.initiate({
     _id: "shard2",
     members: [
-        { _id: 0, host: "shard2:27019" }
+        { _id: 0, host: "shard2_1:27019" },
+        { _id: 1, host: "shard2_2:27019" },
+        { _id: 2, host: "shard2_3:27019" }
     ]
 });
 EOF
 
-echo "Shard2 initialized."
+echo "Shard2 replication set initialized."
 
 # Настройка роутера и заполнение тестовыми данными
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
-sh.addShard("shard1/shard1:27018");
-sh.addShard("shard2/shard2:27019");
+sh.addShard("shard1/shard1_1:27018,shard1_2:27018,shard1_3:27018");
+sh.addShard("shard2/shard2_1:27019,shard2_2:27019,shard2_3:27019");
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name": "hashed" });
 
